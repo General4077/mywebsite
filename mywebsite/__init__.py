ec683dd4378e6cac51381dda8b41e5cf020bf40c
Jayson Scruggs <jayson.scruggs@gmail.com>
2023-11-13 16:04:26 -0600

    reinit

--- mywebsite/__init__.py
+++
@@ -1,6 +0,0 @@
-from .app import app_factory
-
-app = app_factory()
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
--- mywebsite/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .blueprints.base.mywebsite import base
-
-app = Flask(__name__)
-
-
-def app_factory():
-    app.register_blueprint(base)
-    return app
